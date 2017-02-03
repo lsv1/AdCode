8c49b60a714aa7d4feb20c477ca281ed9c12d8da
lsv1 <luissastreverzun@gmail.com>
2017-02-03 12:43:16 -0500

    Create DFP_get_all_active_products_and_their_placement_targeting.py

---
+++ DFP_get_all_active_products_and_their_placement_targeting.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from googleads import dfp
+
+
+def main(client):
+    # Initialize appropriate service.
+
+    product_service = client.GetService('ProductService',
+                                        version='v201611')
+
+    # Create a statement to select products.
+
+    statement = dfp.FilterStatement()
+
+    # Retrieve a small amount of products at a time, paging
+    # through until all products have been retrieved.
+
+    print('product.status,product.id,product.name,targetedPlacementIds')
+    while True:
+        response = product_service.getProductsByStatement(statement.ToStatement())
+        if 'results' in response:
+            for product in response['results']:
+                try:
+                    for item in product.builtInTargeting.inventoryTargeting.targetedPlacementIds:
+                        if product.productType == "DFP" and product.status == "ACTIVE":
+                            print('%s,%d,%s,%s') % (product.status, product.id, product.name, item)
+                except:
+                    pass
+
+            statement.offset += dfp.SUGGESTED_PAGE_LIMIT
+        else:
+            break
+
+    print '\nNumber of results found: %s' \
+          % response['totalResultSetSize']
+
+
+if __name__ == '__main__':
+    # Initialize client object.
+
+    dfp_client = dfp.DfpClient.LoadFromStorage()
+    main(dfp_client)
