53dac43e4f88b2ce2150bc9e88400152511afb9e
lsv1 <luissastreverzun@gmail.com>
2017-02-14 13:10:04 -0500

    Update DFP_get_all_users_teams_and_associations.py

--- DFP_get_all_users_teams_and_associations.py
+++ DFP_get_all_users_teams_and_associations.py
@@ -6,15 +6,12 @@ from googleads import dfp
 def main(client):
 
     user_service = client.GetService('UserService', version='v201611')
-    team_service = client.GetService('TeamService', version='v201611')
     user_team_association_service = client.GetService('UserTeamAssociationService',version='v201611')
-    roles = user_service.getAllRoles()
 
     statement = dfp.FilterStatement()
 
     while True:
         dfp_users = user_service.getUsersByStatement(statement.ToStatement())
-        dfp_teams = team_service.getTeamsByStatement(statement.ToStatement())
         dfp_user_team_assoc = user_team_association_service.getUserTeamAssociationsByStatement(statement.ToStatement())
 
         print('isActive, id, name, email, roleId, roleName, teamId')
@@ -25,7 +22,6 @@ def main(client):
         break
 
 if __name__ == '__main__':
-    # Initialize client object.
 
     dfp_client = dfp.DfpClient.LoadFromStorage()
     main(dfp_client)
