c72bcba575386c56acf2ccdd10625db22ab1f601
lsv1 <luissastreverzun@gmail.com>
2017-02-14 13:00:33 -0500

    Update DFP_get_all_users_teams_and_associations.py

--- DFP_get_all_users_teams_and_associations.py
+++ DFP_get_all_users_teams_and_associations.py
@@ -1,57 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Import appropriate modules from the client library.
 from googleads import dfp
 
 def main(client):
-    # Initialize appropriate service.
 
     user_service = client.GetService('UserService', version='v201611')
     team_service = client.GetService('TeamService', version='v201611')
     user_team_association_service = client.GetService('UserTeamAssociationService',version='v201611')
     roles = user_service.getAllRoles()
 
-    # Create a statement to select users. - Not required, need all users.
-
     statement = dfp.FilterStatement()
-
-    # Retrieve a small amount of users at a time, paging
-    # through until all users have been retrieved.
 
     while True:
         dfp_users = user_service.getUsersByStatement(statement.ToStatement())
         dfp_teams = team_service.getTeamsByStatement(statement.ToStatement())
         dfp_user_team_assoc = user_team_association_service.getUserTeamAssociationsByStatement(statement.ToStatement())
-
-        ''' Commenting out this big block that was used for testing the whole dataset
-        if 'results' in dfp_users and dfp_teams and dfp_user_team_assoc:
-
-            print('Users')
-            for user in dfp_users['results']:
-                # Print out some information for each user.
-                print '%d, %s' % (user['id'], user['name'])
-            statement.offset += dfp.SUGGESTED_PAGE_LIMIT
-
-            print('Teams')
-            for team in dfp_teams['results']:
-                # Print out some information for each team.
-                print('%d, %s' % (team['id'],team['name']))
-            statement.offset += dfp.SUGGESTED_PAGE_LIMIT
-
-            print('User and Team Associations')
-            for user_team_association in dfp_user_team_assoc['results']:
-                # Print out some information for each user team association.
-                print('%d, %d' % (user_team_association['teamId'],user_team_association['userId']))
-            statement.offset += dfp.SUGGESTED_PAGE_LIMIT
-        else:
-            break
-
-        print('Roles')
-        for role in roles:
-            print '%d, %s' % (role['id'], role['name'])
-
-        '''
 
         print('isActive, id, name, email, roleId, roleName, teamId')
         for user_check in dfp_users['results']:
