816989381f2d54e8aae1ed6bd6b62681c46bab9a
lsv1 <luissastreverzun@gmail.com>
2017-03-09 11:31:44 -0500

    Update DFP_active_placements_and_targeted_ad_unit_ids.py
    Returns everything now

--- DFP_active_placements_and_targeted_ad_unit_ids.py
+++ DFP_active_placements_and_targeted_ad_unit_ids.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python
-# _*_ coding:utf-8 _*_
+# -*- coding: utf-8 -*-
+# I'm dropping all non ascii characters from product names because it's easy, this isn't great if you're a non-American DFP user.
+# You may have to consider encoding fixes if you need special characters in your product names.
 
 from googleads import dfp
 import csv
-# Unicode might spit out errors if you have none-breaking spaces in your product or inventory names, I recommend fixing this at the source instead of hackery, but whatever.
-#import unicodecsv as csv
+
 
 def main(client):
+    placement_service = client.GetService('PlacementService', version='v201702')
 
-  placement_service = client.GetService('PlacementService', version='v201702')
-  ad_unit_service = client.GetService('InventoryService', version='v201702')
-  query = 'WHERE status = :status'
-  values = [
-      {'key': 'status',
-       'value': {
-           'xsi_type': 'TextValue',
-           'value': 'ACTIVE'
-       }},
-  ]
-  statement = dfp.FilterStatement(query, values)
+    statement = dfp.FilterStatement()
 
-  with open('dfp_placements.csv', 'wb') as csvfile:
-    fieldnames = ['placement_id', 'placement_name', 'ad_unit_id', 'ad_unit_name']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+    with open('dfp_placements.csv', 'wb') as csvfile:
+        fieldnames = ['placement_status', 'placement_id', 'placement_name', 'ad_unit_id']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-  print ', '.join(fieldnames)  # Stupid way to print arrays.
+    print ', '.join(fieldnames)  # Stupid way to print this IMO. Seems unpythonic.
 
-  while True:
-    dfp_placements = placement_service.getPlacementsByStatement(statement.ToStatement())
-    dfp_ad_units = ad_unit_service.getAdUnitsByStatement(statement.ToStatement())
+    while True:
+        response = placement_service.getPlacementsByStatement(statement.ToStatement())
+        with open('dfp_placements.csv', 'ab') as csvfile:
+            if 'results' in response:
+                for placement in response['results']:
+                    try:
+                        for ad_unit_id in placement.targetedAdUnitIds:
+                            print('%d,%s,%s,%s') % (placement.id, placement.status, placement.name.encode('ascii', 'ignore'), ad_unit_id)
+                            csv_writer = csv.writer(csvfile, delimiter=',')
+                            csv_writer.writerow([placement.id, placement.status, placement.name.encode('ascii', 'ignore'), ad_unit_id])
 
-    if 'results' in dfp_placements and dfp_ad_units:
-      for placement in dfp_placements['results']:
-        for adunit in dfp_ad_units['results']:
-          for targetedAdUnit in placement.targetedAdUnitIds:
-            if adunit.id == targetedAdUnit:
-              print('%d,%s,%s,%s' % (placement['id'], placement['name'], adunit.id, adunit.name))
-              with open('dfp_placements.csv', 'ab') as csvfile:
-                csv_writer = csv.writer(csvfile, delimiter=',')
-                csv_writer.writerow([placement.id, placement.name, adunit.id, adunit.name])
-      statement.offset += dfp.SUGGESTED_PAGE_LIMIT
-    else:
-      break
+                    except:
+                        print('%d,%s,%s,%s') % (
+                        placement.id, placement.status, placement.name.encode('ascii', 'ignore'), "N/A")
+                        csv_writer = csv.writer(csvfile, delimiter=',')
+                        csv_writer.writerow([placement.id, placement.status, placement.name.encode('ascii', 'ignore'), "N/A"])
+                        pass
+                statement.offset += dfp.SUGGESTED_PAGE_LIMIT
+
 
 if __name__ == '__main__':
-  dfp_client = dfp.DfpClient.LoadFromStorage()
-  main(dfp_client)
+    dfp_client = dfp.DfpClient.LoadFromStorage()
+    main(dfp_client)
