0eb4154da33552c6cdcaaf46218bf6d525d35844
Luis <luissastreverzun@gmail.com>
2017-08-09 19:47:10 -0400

    Update ADS.txt_crawler.py

--- ADS.txt_crawler.py
+++ ADS.txt_crawler.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-# Working on an implementation that's better than the IAB one because that one, while it lays out each piece of functionality, breaks whenever a site returns funky stuff.
 
 import csv
 import requests
 import eventlet
+eventlet.sleep() # Fix for https://github.com/eventlet/eventlet/issues/401 in Debian
 from collections import defaultdict
 
 eventlet.monkey_patch()
 
 domain_source = 'https://moz.com/top500/domains/csv'
 domains = "top_500_domains.csv"
+output_file = "ads.txt"
 
 
 def download_domains():
-    print "Downloading Source Domains"
     r = requests.get(domain_source)
     with open(domains, "wb") as code:
         code.write(r.content)
@@ -27,14 +27,22 @@ def scrape_domains(input_domains):
             for (k, v) in row.items():
                 columns[k].append(v)
 
+    open(output_file, "w").close()
     for site in columns['URL']:
         try:
             with eventlet.Timeout(10):
                 r = requests.get(("http://" + site + "ads.txt"), allow_redirects=1)
+                print "Scraped " + r.url
                 if (r.status_code == 200) and (r.headers['content-type'] == 'text/plain'):
-                    print r.url + "\n", r.content
-                    # print site + "ads.txt"
+                    with open(output_file, 'ab') as f:
+                        f.write(r.url)
+                        f.write("\n")
+                        for chunk in r.iter_content(chunk_size=1024):
+                            if chunk:  # filter out keep-alive new chunks
+                                f.write(chunk)
+                                f.write("\n")
         except:
+            print "Timed out scraping " + r.url
             pass
 
 
