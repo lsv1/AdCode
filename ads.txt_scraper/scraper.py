2b72eedd38c3bc34578ad67bcfc8ef41547c5be9
Luis <luissastreverzun@gmail.com>
2018-01-29 15:16:20 -0500

    Update scraper.py

--- ads.txt_scraper/scraper.py
+++ ads.txt_scraper/scraper.py
@@ -9,13 +9,18 @@ import datetime
 
 eventlet.sleep()  # Fix for https://github.com/eventlet/eventlet/issues/401 in Debian
 
-domains_file = "top-1000-sites.csv"
+domains_file = "top-10000-sites.csv"
 adstxt_entry = "kargo"
 output_file = "output_file.txt"
 
-with open(output_file, "a") as f:
-    f.write("Starting Scrape on " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
-    print("Starting Scrape on " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
+with open(output_file, "w") as f:
+    f.write(
+        "Scraping for ads.txt in " + domains_file + " for " + adstxt_entry + " on " + datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S") + "\n")
+    f.write("domain,ssp,in_ads_txt,date\n")
+    print(
+            "Scraping for ads.txt in " + domains_file + " for " + adstxt_entry + " on " + datetime.datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S") + "\n")
 
 
 def scrape_domains(input_domains):
@@ -32,24 +37,27 @@ def scrape_domains(input_domains):
             for (k, v) in row.items():
                 columns[k].append(v)
 
-    with open(output_file, "a") as f:
-        for site in columns["URL"]:
-            with eventlet.Timeout(3):
+    for site in columns["URL"]:
+        with open(output_file, "a") as f:
+            with eventlet.Timeout(2):
                 try:
                     r = requests.get(("http://" + site + "/ads.txt"), allow_redirects=1, headers=adstxt_headers,
-                                     timeout=3)
+                                     timeout=2)
                     if r.status_code == requests.codes.ok:
                         if adstxt_entry in r.content:
-                            f.write(site + ": found " + adstxt_entry + ".\n")
+                            f.write(site + "," + adstxt_entry + "," + "yes" + "," + datetime.datetime.now().strftime(
+                                "%Y-%m-%d %H:%M:%S") + "\n")
                             print(site + ": found " + adstxt_entry + ".")
                     else:
                         pass
-                        f.write(site + ": nothing found.\n")
+                        f.write(site + "," + adstxt_entry + "," + "no" + "," + datetime.datetime.now().strftime(
+                            "%Y-%m-%d %H:%M:%S") + "\n")
                         print(site + ": nothing found.")
 
                 except:
                     pass
-                    f.write(site + ": nothing found or error.\n")
+                    f.write(site + "," + adstxt_entry + "," + "no" + "," + datetime.datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + "\n")
                     print(site + ": nothing found or error.")
 
 
